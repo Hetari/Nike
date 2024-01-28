f8838d440f246122de0aca6dd750f01ea92dac6d
Hetari <hetari4py@gmail.com>
2024-01-29 00:33:33 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <h1 class="text-5xl text-red-900">hi</h1>
+    <div class="wrapper">
+      <nav></nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
