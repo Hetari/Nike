2852ca7a591d78ddd2544aad7208e91be5d720de
Hetari <hetari4py@gmail.com>
2024-01-29 01:07:09 +0300

    Finish the import

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
   <main class="relative">
     <Nav />
-    <section class="xl:padding-1 wide:padding-r padding-b">Hero</section>
+    <section class="xl:padding-1 wide:padding-r padding-b"><Hero /></section>
 
-    <section class="padding">PopularProducts</section>
+    <section class="padding"><PopularProducts /></section>
 
-    <section class="padding">SuperQuality</section>
+    <section class="padding"><SuperQuality /></section>
 
-    <section class="padding-x py-10">Services</section>
+    <section class="padding-x py-10"><Services /></section>
 
-    <section class="padding">SpacialOffer</section>
+    <section class="padding"><SpacialOffer /></section>
 
-    <section class="bg-pale-blue padding">CustomerReviews</section>
+    <section class="bg-pale-blue padding"><CustomerReviews /></section>
 
-    <section class="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+    <section class="padding-x sm:py-32 py-16 w-full"><Subscribe /></section>
 
-    <section class="bg-black padding-x padding-t pb-8">Footer</section>
+    <section class="bg-black padding-x padding-t pb-8"><Footer /></section>
   </main>
 </template>
 
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import Nav from "@/components/Nav.vue";
-import CustomerReviews from "@/components/CustomerReviews.vue";
-import Footer from "@/components/Footer.vue";
-import PopularProducts from "@/components/PopularProducts.vue";
-import Services from "@/components/Services.vue";
-import SpacialOffer from "@/components/SpacialOffer.vue";
-import Subscribe from "@/components/Subscribe.vue";
-import SuperQuality from "@/components/SuperQuality.vue";
+import {
+  Nav,
+  Hero,
+  CustomerReviews,
+  Footer,
+  PopularProducts,
+  Services,
+  SpacialOffer,
+  Subscribe,
+  SuperQuality,
+} from "@/components";
 </script>
---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import Nav from "@/components/Nav.vue";
+import Hero from "@/components/Hero.vue";
+import CustomerReviews from "@/components/CustomerReviews.vue";
+import Footer from "@/components/Footer.vue";
+import PopularProducts from "@/components/PopularProducts.vue";
+import Services from "@/components/Services.vue";
+import SpacialOffer from "@/components/SpacialOffer.vue";
+import Subscribe from "@/components/Subscribe.vue";
+import SuperQuality from "@/components/SuperQuality.vue";
+
+export {
+  Nav,
+  Hero,
+  CustomerReviews,
+  Footer,
+  PopularProducts,
+  Services,
+  SpacialOffer,
+  Subscribe,
+  SuperQuality,
+};
