628da1e54c6cdf2b8b96317e56c14b64ffddf6b8
Hetari <hetari4py@gmail.com>
2024-02-11 22:41:01 +0300

    fix the import bug

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      // path: "/about",
+      // name: "about",
+      // // route level code-splitting
+      // // this generates a separate chunk (About.[hash].js) for this route
+      // // which is lazy-loaded when the route is visited.
+      // component: () => import("../views/AboutView.vue"),
     },
   ],
 });
