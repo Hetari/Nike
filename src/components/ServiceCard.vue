72e330da5738c240deac540988d03fb50f9bfab0
Hetari <hetari4py@gmail.com>
2024-02-20 21:20:35 +0300

    Finish the services section

---
+++ src/components/ServiceCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="flex flex-col flex-1 sm:w[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16"
+  >
+    <div
+      class="w-11 h-11 flex justify-center items-center bg-coral-red rounded-full"
+    >
+      <img :src="service.imgURL" :alt="service.label" width="24" height="24" />
+    </div>
+    <h3 class="mt-5 font-palanquin text-3xl leading-normal font-bold">
+      {{ service.label }}
+    </h3>
+    <p class="mt-3 font-montserrat info-text">
+      {{ service.subtext }}
+    </p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  service: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
--- src/sections/Services.vue
+++ src/sections/Services.vue
@@ -1,9 +1,14 @@
 <template>
-  <div>Services</div>
+  <section class="max-container flex justify-center flex-wrap gap-9">
+    <ServiceCard
+      v-for="service in services"
+      :key="service.id"
+      :service="services[service.id]"
+    />
+  </section>
 </template>
 
-<script>
-export default {
-  setup() {},
-};
+<script setup>
+import { ServiceCard } from "@/components";
+import { services } from "@/constants";
 </script>
